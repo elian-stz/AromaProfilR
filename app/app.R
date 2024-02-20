6335e2f500e5ecc566ece3a4c237ce305c33d06a
Elian Strozyk <elian.strozyk@etu.umontpellier.fr>
2024-02-20 10:11:56 +0100

    fixed .gitignore

---
+++ app/app.R
@@ -0,0 +1,55 @@
+library(shiny)
+library(shinymanager)
+library(RSQLite)
+
+ui <- fluidPage(
+  titlePanel("Test authentification"),
+  uiOutput("myinput"),
+  textOutput("role"),
+  downloadLink("download", "Download RData")
+)
+
+# Wrap your UI with secure_app
+ui <- secure_app(ui,
+                 enable_admin = TRUE,
+                 tags_bottom = tags$div(
+                   tags$p(
+                     "For any question, please contact the ",
+                     tags$a(
+                       href = "mailto:someone@example.com",
+                       target="_top", "PTV"
+                     )
+                   )
+                 )
+                )
+
+server <- function(input, output, session) {
+  # call the server part
+  # check_credentials returns a function to authenticate users
+  res_auth <- secure_server(
+    check_credentials = check_credentials(
+      db = "./login_database.sqlite",
+      passphrase = "testpass"
+    )
+  )
+
+  output$myinput <- renderUI({
+    output$download <- downloadHandler(
+      filename = function() {
+        paste("iris_", Sys.Date(), ".Rdata", sep='')
+      },
+      content = function(con) {
+        save(iris, file=con)
+      }
+    )
+    #output$download = load(file=paste("iris", Sys.Date(), ".Rdata"))
+    if (reactiveValuesToList(res_auth)$admin == TRUE) {
+      output$role <- renderText("Your role is admin")
+    } else {
+      output$role <- renderText("Your role is user here")
+    }
+    
+  })
+}
+
+shinyApp(ui, server)
